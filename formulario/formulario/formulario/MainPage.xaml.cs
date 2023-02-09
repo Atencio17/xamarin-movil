4c176af8029f9b010270c28bf1ce764cd1040065
Atencio17 <atenciomuniveandresfelipe2@gmail.com>
2023-02-09 17:44:36 -0500

    formulario

---
+++ formulario/formulario/formulario/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace formulario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registrar(object sender, EventArgs e)
+        {
+            DisplayAlert("¡Registro completado!","Usted se ha registrado con exito","Ok");
+        }
+    }
+}
